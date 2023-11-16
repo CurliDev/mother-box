b8ecf0dc7a26a75aeebd42e290b9d5c2cbe21284
NoobDeveloper412 <mzuhair412@gmail.com>
2023-11-16 22:46:17 +0500

    Created some more components

---
+++ src/statics/icons/add.tsx
@@ -0,0 +1,28 @@
+function AddIcon() {
+  return (
+    <svg
+      width="80"
+      height="80"
+      viewBox="0 0 80 80"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M20 40H60"
+        stroke="#A072DF"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M40 60V20"
+        stroke="#A072DF"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default AddIcon;
